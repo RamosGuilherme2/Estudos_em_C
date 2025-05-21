cee8f9904c93f8a6a638ce408644cf6f8118d603
RamosGuilherme2 <guilhermealmeida4445@gmail.com>
2025-05-21 04:04:01 -0300

    Exercicios do dia 20/05/2025

---
+++ Exercicios20_05/ArraysEmC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int notas[5] = {7, 8, 6, 9, 10};
+    int soma = 0;
+
+    for(int i = 0; i < 5; i++) {
+        soma += notas[i];
+    }
+    float media = soma / 5.0;
+    printf("Média das notas = %.2f\n", media);
+    return 0;
+}
